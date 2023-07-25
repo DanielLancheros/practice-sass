fba8f4f498291d1cc4a237214dc895d7ea436025
Daniel Lancheros <danielcoding327@gmail.com>
2023-07-25 15:35:13 -0400

    starting animations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Box from './components/Box';
+import BoxRace from './components/BoxRace/BoxRace';
 import './styles/App.scss'
 
 function App() {
   return (
     <>
     <Box />
+    <BoxRace />
     </>
   );
 }
